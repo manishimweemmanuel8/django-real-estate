d19623d62a93aa7ce964333a67ce14834418a00b
emanishimwe <manishimweemmanuel8@gmail.com>
2022-09-28 16:34:42 +0200

    feat: add customer user model

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-# admin.site.site_header = "Real Estate Admin"
-# admin.site.site_title = "Real Estate Admin Portal"
-# admin.site.index_title = "Welcome to the Real Estate Portal"
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real Estate Admin Portal"
+admin.site.index_title = "Welcome to the Real Estate Portal"
