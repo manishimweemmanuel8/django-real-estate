7d0e3af2e25acef8439c5cdfed490d0eb1c17b02
emanishimwe <manishimweemmanuel8@gmail.com>
2022-09-29 16:48:04 +0200

    feat: user auth-register, login,password, resetP

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -5,8 +5,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("supersecret/", admin.site.urls),
-    # path("api/v1/auth/", include("djoser.urls")),
-    # path("api/v1/auth/", include("djoser.urls.jwt")),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
     # path("api/v1/profile/", include("apps.profiles.urls")),
     # path("api/v1/properties/", include("apps.properties.urls")),
     # path("api/v1/ratings/", include("apps.ratings.urls")),
