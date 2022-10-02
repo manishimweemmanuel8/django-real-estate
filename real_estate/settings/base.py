9227601b2455e36d005bce1cd10873e996990b88
emanishimwe <manishimweemmanuel8@gmail.com>
2022-10-02 11:51:20 +0200

    Feat:test akk routes work in container

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -29,12 +29,13 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 # Application definition
 
 DJANGO_APPS = [
+    "django.contrib.contenttypes",
     "django.contrib.admin",
     "django.contrib.auth",
-    "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites"
 ]
 
 SITE_ID = 1
@@ -46,6 +47,7 @@ THIRD_PARTY_APPS = [
     "phonenumber_field",
     "djoser",
     "rest_framework_simplejwt",
+    "djcelery_email",
 ]
 
 LOCAL_APPS = [
